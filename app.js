0dbb1976eff300f26b3905ed2d22f8655566fb34
shotamik <shotamikeladze@gmail.com>
2022-11-07 17:19:39 +0400

    added last task

--- app.js
+++ app.js
@@ -21,3 +21,66 @@ const box = document.getElementById('box');
 
 
 box.appendChild(image);
+
+const divSection = document.querySelector(".divclass")
+
+const characters = [
+	{
+		first_name: "Harry",
+		last_name: "Potter",
+		house: "Gryffindor",
+		image: "https://hp-api.herokuapp.com/images/harry.jpg",
+		actor: "Daniel Radcliffe",
+	},
+	{
+		first_name: "Hermione",
+		last_name: "Granger",
+		house: "Gryffindor",
+		image: "https://hp-api.herokuapp.com/images/hermione.jpeg",
+		actor: "Emma Watson",
+	},
+	{
+		first_name: "Ron",
+		last_name: "Weasley",
+		house: "Gryffindor",
+		image: "https://hp-api.herokuapp.com/images/ron.jpg",
+		actor: "Rupert Grint",
+	},
+	{
+		first_name: "Draco",
+		last_name: "Malfoy",
+		house: "Slytherin",
+		image: "https://hp-api.herokuapp.com/images/draco.jpg",
+		actor: "Tom Felton",
+	},
+];
+
+
+
+function renderCharacters(arr) {
+    const renderedCards = characters.map(char => {
+        return `
+            <div class="divclass1">
+                
+                <div class="divtext">
+                    <h1>${char.first_name} ${char.last_name}</h1>
+                    <div class="divclass2">
+                        <h1 class="house"> House: </h1>
+                        <h3>${char.house}</h3>
+                    </div>
+                    
+                </div>
+                <img src="${char.image}" class="card-img"  />
+            </div>
+        `
+    })
+
+
+
+    console.log(renderedCards.join(""));
+    divSection.innerHTML = renderedCards.join("");
+
+}
+
+
+renderCharacters();
