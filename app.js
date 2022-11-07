d65a9247aabb07099efbc2a074cdfae5a3d2bc21
shotamik <shotamikeladze@gmail.com>
2022-11-07 17:17:28 +0400

    added img task

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ function handleclick() {
 }
 
 btn.addEventListener("click", handleclick);
+
+const image = document.createElement('img');
+
+
+
+image.setAttribute(
+    "src",
+    "https://picsum.photos/id/1/200/300",
+)
+
+const box = document.getElementById('box');
+
+
+box.appendChild(image);
